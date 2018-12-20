ecd0921c1f7c8fa44d2bfd1d25a5ef7ace328d35
Gustavo Costa <gustavolegal7@gmail.com>
2018-12-20 13:11:08 -0400

    criado rotas, modulo, model de tarefas, componente listar

---
+++ gerenciador-de-tarefa/src/app/tarefas/tarefas.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { TarefaService } from "./shared";
+import { ListarTarefasComponent } from "./listar";
+@NgModule({
+  declarations: [ListarTarefasComponent],
+  imports: [CommonModule],
+  providers: [TarefaService]
+})
+export class TarefasModule {}
