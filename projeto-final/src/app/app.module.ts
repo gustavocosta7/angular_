0c94449eb55df2936a19b9909052fe2abc653e6a
Gustavo de Almeida Costa <gustavolegal7@gmail.com>
2019-01-11 13:50:15 -0400

    integrado dashboard e calculadora

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {DashboardModule} from './dashboard';
+import {CalculadoraModule} from './calculadora/calculadora.module';
 
 @NgModule({
   declarations: [
@@ -11,8 +12,10 @@ import {DashboardModule} from './dashboard';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    DashboardModule
+    DashboardModule,
+    CalculadoraModule,
+    AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
